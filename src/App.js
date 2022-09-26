2d9a98c5b07f74e4709793b9b7fac6c09358561e
JamyPut <jp2001dj@gmail.com>
2022-09-26 22:14:55 +0200

    added pages and tested routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
-
+import {Link, Route} from "react-router-dom";
+import HomePage from "./pages/home";
 function App() {
   // We houden in de state bij of iemand is "ingelogd" (simpele versie)
   const [isAuthenticated, toggleIsAuthenticated ] = useState(false);
@@ -8,6 +9,9 @@ function App() {
   return (
     <div>
       Maak hier jouw prachtige blog-applicatie!
+       <Route exact path={"/"}>
+           <HomePage/>
+       </Route>
     </div>
   );
 }
---
+++ src/pages/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+function Home(){
+
+    return(
+        <p>Hoi hoe gaat het</p>
+    )
+}
+export default Home;
